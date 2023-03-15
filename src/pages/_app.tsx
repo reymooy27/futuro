001e7da70ad4be35b2fcd0bf0d38f0f243a3512b
reymooy27 <gdrrey@gmail.com>
2023-03-16 00:28:21 +0800

    add chakra provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,23 @@
 import { AppProps, type AppType } from "next/app";
 import { type Session } from "next-auth";
-import { SessionProvider } from "next-auth/react";
+import { SessionProvider, useSession } from "next-auth/react";
 
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 import { ReactElement } from "react";
 import { NextPage } from "next";
+import { ChakraProvider } from "@chakra-ui/react";
 
 
 export type NextPageWithLayout = NextPage & {
-  getLayout?: (page: ReactElement) => ReactElement
+  getLayout?: (page: ReactElement) => ReactElement,
+  auth?: boolean
+
 }
 
 type AppPropsWithLayout = AppProps & {
-  Component: NextPageWithLayout
+  Component: NextPageWithLayout,
 }
 
 
@@ -27,9 +30,32 @@ const MyApp: AppType<{ session: Session | null }> = ({
 
   return (
     <SessionProvider session={session}>
-      {getLayout(<Component {...pageProps} />)}
+      <ChakraProvider>
+        {Component.auth ? (
+          <Auth>
+            {getLayout(<Component {...pageProps} />)}
+          </Auth>
+        ) : (
+          <>
+          {getLayout(<Component {...pageProps} />)}
+          </>
+        )}
+      </ChakraProvider>
     </SessionProvider>
   );
 };
+
+function Auth({ children }: { children: JSX.Element }) {
+  // if `{ required: true }` is supplied, `status` can only be "loading" or "authenticated"
+  const { status } = useSession({
+    required: true,
+  });
+
+  if (status === "loading") {
+    return (<div>Loading...</div>)
+  }
+
+  return children;
+}
 
 export default api.withTRPC(MyApp);
