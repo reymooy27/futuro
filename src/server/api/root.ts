d8397582b4b0d90eb0450222c82927d2dcc1be01
reymooy27 <gdrrey@gmail.com>
2023-03-24 01:52:16 +0800

    add team & tournament router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,7 @@
 import { createTRPCRouter } from "~/server/api/trpc";
 import { exampleRouter } from "~/server/api/routers/example";
+import { teamRouter } from "./routers/teams";
+import { tournamentRouter } from "./routers/tournament";
 
 /**
  * This is the primary router for your server.
@@ -8,6 +10,8 @@ import { exampleRouter } from "~/server/api/routers/example";
  */
 export const appRouter = createTRPCRouter({
   example: exampleRouter,
+  team: teamRouter,
+  tournament: tournamentRouter
 });
 
 // export type definition of API
