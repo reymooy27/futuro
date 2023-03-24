12435b266e043129a4823d5f1a891629481b6c4b
reymooy27 <gdrrey@gmail.com>
2023-03-25 00:00:29 +0800

    all tournament data

--- src/pages/tournament/all.tsx
+++ src/pages/tournament/all.tsx
@@ -1,14 +1,25 @@
 import Layout from '~/components/Layout'
 import Head from 'next/head'
 import React, { ReactElement } from 'react'
+import { api } from '~/utils/api'
+import Link from 'next/link'
 
 export default function AllTournament() {
+
+  const {data: allTournament} = api.tournament.getAll.useQuery()
+
   return (
     <>
       <Head>
         <title>Turnamen</title>
       </Head>
-      <div>All</div>
+      <div>
+      {allTournament?.map((tournament)=>(
+        <Link key={tournament?.id} href={`/tournament/${tournament?.id}`} className='min-w-[200px] h-[100px] rounded-xl border border-border hover:opacity-[0.2] transition-all ease-in-out duration-300'>
+          <div>{tournament?.name}</div>
+        </Link>
+      ))}
+      </div>
     </>
   )
 }
