80789b0d41c056656974b9afeacac65ce491529e
reymooy27 <gdrrey@gmail.com>
2023-03-27 00:47:07 +0800

    add tournaments

--- src/pages/team/[id].tsx
+++ src/pages/team/[id].tsx
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import { useRouter } from 'next/router'
 import React, { ReactElement } from 'react'
 import { api } from '~/utils/api'
+import Link from 'next/link'
 
 export default function Match() {
 
@@ -17,6 +18,14 @@ export default function Match() {
       </Head>
      <div>
       <h1>{data?.name}</h1>
+      <div className="mt-3">
+        <h1 className="font-bold text-xl">Turnamen</h1>
+        <div className='flex gap-3'>
+          {data?.tournaments?.map(tournament=>(
+            <Link href={`/tournament/${tournament?.id}`} className='p-3 bg-slate-300 rounded' key={tournament?.id}>{tournament?.name}</Link>
+          ))}
+        </div>
+      </div>
      </div>
     </>
   )
