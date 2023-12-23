740d7b77dd5579b29646d078480d398311a0ebd2
reymooy27 <gdrrey@gmail.com>
2023-12-23 16:49:16 +0800

    adding menu onclick profile

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,6 +3,17 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import {Bell, Search} from 'react-feather'
+import {
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  // MenuGroup,
+  // MenuOptionGroup,
+  // MenuDivider,
+  Button
+} from '@chakra-ui/react'
+
 export default function Navbar() {
   const {data:session} = useSession()
   return (
@@ -14,17 +25,40 @@ export default function Navbar() {
         <input 
           type="text" 
           placeholder='Search for players...' 
-          className='rounded-md text-[14px] px-3 pl-9 w-full outline-none h-[40px] border-gray-400 border-[0.5px] before:content-[  ]'/>
+          className='rounded-md text-[14px] px-3 pl-9 w-full 
+          outline-none h-[40px] border-gray-400 border-[0.5px] before:content-[  ]'/>
       </div>
       <div className='flex justify-center items-center gap-3'>
         <Link href={'/notification'}>
           <Bell width={30}/>
         </Link>
         <div className='flex justify-center items-center gap-2'>
-          {session ? <Link className='flex items-center gap-3' href={'/profile'}>
-            <Image className='rounded-full' src={session?.user?.image as string} height={30} width={30} alt='user-profile'/>
-            <h3 className='hidden md:block'>{session?.user?.name}</h3>
-          </Link> : <Link href='/login'>Login</Link>}
+          {session ? 
+            <span className='flex items-center gap-3'>
+              <Menu>
+                <MenuButton className='flex gap-3' flex={1}>
+                  <Image className='rounded-full' 
+                          src={session?.user?.image as string} 
+                          height={30} width={30} alt='user-profile'
+                  />
+                  {/* <h3 className='hidden md:block'>{session?.user?.name}</h3> */}
+                </MenuButton>
+                <MenuList>
+                  <MenuItem>
+                    <Link href='/profile'>
+                      Profile
+                    </Link>
+                  </MenuItem>
+                  <MenuItem>Create a Copy</MenuItem>
+                  <MenuItem>Mark as Draft</MenuItem>
+                  <MenuItem>Delete</MenuItem>
+                  <MenuItem>Attend a Workshop</MenuItem>
+                </MenuList>
+              </Menu>
+            </span> 
+            : 
+            <Link href='/login'>Login</Link>
+          }
         </div>
       </div>
     </header>
