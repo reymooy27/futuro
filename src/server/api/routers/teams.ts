edc1d222ac82dc7b6e1c3ecceee5071689b919e1
reymooy27 <gdrrey@gmail.com>
2024-03-16 19:14:16 +0800

    create a register instead of directly put team on tournament

--- src/server/api/routers/teams.ts
+++ src/server/api/routers/teams.ts
@@ -8,119 +8,121 @@ import {
 } from "~/server/api/trpc";
 
 export const teamRouter = createTRPCRouter({
-
   getAll: publicProcedure.query(({ ctx }) => {
     try {
       return ctx.prisma.team.findMany();
     } catch (error) {
       throw new TRPCError({
-        code: 'BAD_REQUEST',
-        message: 'There is something wrong'
-      })
+        code: "BAD_REQUEST",
+        message: "There is something wrong",
+      });
     }
   }),
 
-  getTeamById: publicProcedure
-  .input(z.number())
-  .query(({ ctx, input }) => {
+  getTeamById: publicProcedure.input(z.number()).query(({ ctx, input }) => {
     try {
       const team = ctx.prisma.team.findUnique({
-        where:{
-          id: input
+        where: {
+          id: input,
         },
-        include:{
+        include: {
           awayMatches: true,
           homeMatches: true,
           players: true,
-          tournaments:{
-            select:{
+          tournaments: {
+            select: {
               id: true,
-              name: true
-            }
-          }
-        }
+              name: true,
+            },
+          },
+        },
       });
-      return team
+      return team;
     } catch (error) {
       throw new TRPCError({
-        code: 'BAD_REQUEST',
-        message: 'There is something wrong'
-      })
+        code: "BAD_REQUEST",
+        message: "There is something wrong",
+      });
     }
   }),
 
-  getMyTeam: protectedProcedure.query(({ctx})=>{
+  getMyTeam: protectedProcedure.query(({ ctx }) => {
     try {
       return ctx.prisma.team.findMany({
-        where:{
-          userId: parseInt(ctx.session.user.id, 10)
-        } 
-      })
+        where: {
+          userId: parseInt(ctx.session.user.id, 10),
+        },
+      });
     } catch (error) {
       throw new TRPCError({
-        code: 'BAD_REQUEST',
-        message: 'There is something wrong'
-      })
+        code: "BAD_REQUEST",
+        message: "There is something wrong",
+      });
     }
   }),
-  
+
   createTeam: protectedProcedure
-  .input(z.object({name: z.string()}))
-  .mutation(({input, ctx})=>{
-    try {
-      const team = ctx.prisma.team.create({
-        data:{
-          name: input.name,
-          userId: parseInt(ctx.session.user.id, 10)
-        }
-      })
-      return team
-    } catch (error) {
-      throw new TRPCError({
-        code: 'BAD_REQUEST',
-        message: 'There is something wrong'
-      })
-    }
-  }),
+    .input(z.object({ name: z.string() }))
+    .mutation(({ input, ctx }) => {
+      try {
+        const team = ctx.prisma.team.create({
+          data: {
+            name: input.name,
+            userId: parseInt(ctx.session.user.id, 10),
+          },
+        });
+        return team;
+      } catch (error) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "There is something wrong",
+        });
+      }
+    }),
 
   addTeamToTournament: protectedProcedure
-  .input(z.object({
-    teamId: z.number(),
-    tournamentId: z.number()
-  }))
-  .mutation(async ({input, ctx})=>{
-    try {
-      const registered = await ctx.prisma.team.findFirst({
-        where:{
-          id: input.teamId,
-          tournaments:{
-            some:{
-              id: input.tournamentId
-            }
-          }
-        }
+    .input(
+      z.object({
+        teamId: z.number(),
+        tournamentId: z.number(),
       })
+    )
+    .mutation(async ({ input, ctx }) => {
+      try {
+        const registered = await ctx.prisma.teamRegisterTournament.findFirst({
+          where: {
+            teamId: input.teamId,
+            tournamentId: input.tournamentId,
+          },
+        });
 
-      if(registered) return 'Tim sudah terdaftar'
+        if (registered) return "Tim sudah terdaftar";
 
-      await ctx.prisma.team.update({
-        where:{
-          id: input.teamId
-        },
-        data:{
-          tournaments:{
-            connect:{
-              id: input.tournamentId
-            }
-          }
-        }
-      })
-      return 'Berhasil'
-    } catch (error) {
-      throw new TRPCError({
-        code: 'BAD_REQUEST',
-        message: 'There is something wrong'
-      })
-    }
-  })
+        await ctx.prisma.teamRegisterTournament.create({
+          data: {
+            teamId: input.teamId,
+            tournamentId: input.tournamentId,
+            status: "PENDING",
+          },
+        });
+        // await ctx.prisma.team.update({
+        //   where: {
+        //     id: input.teamId,
+        //   },
+        //   data: {
+        //     tournaments: {
+        //       connect: {
+        //         id: input.tournamentId,
+        //       },
+        //     },
+        //   },
+        // });
+        return "Berhasil Mendaftarkan tim ke turnamen";
+      } catch (error) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "There is something wrong",
+        });
+      }
+    }),
 });
